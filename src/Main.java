ec9ecacbd99d15771e0793d03b9bcac6d4e18ec8
lullii <soylpe20@gmail.com>
2023-06-30 18:07:45 +0300

    mistakes2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Main {
-    public static final String TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+    public static final String TEXT = " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+            " Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     public static void main(String[] args) {
         char[] elements = TEXT.toLowerCase().toCharArray();
@@ -15,7 +16,7 @@ public class Main {
                 if (count == null) {
                     symbols.put(elements[i], 1);
                 } else {
-                    symbols.put(elements[i], count++);
+                    symbols.put(elements[i], ++count);
                 }
             }
         }
@@ -49,7 +50,7 @@ public class Main {
     }
 
     private static Integer getMin(Map<Character, Integer> symbols) {
-        int min = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
 
         for (Map.Entry<Character, Integer> symbol : symbols.entrySet()) {
             if (min > symbol.getValue()) {
